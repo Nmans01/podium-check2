c2e1ac8a8cb425bedea883a7c554cca4880ae62b
nmans01 <natemans01@gmail.com>
2023-03-28 11:56:11 -0400

    Define page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Routes, Route } from "@solidjs/router";
+import { lazy } from "solid-js";
+const Header      = lazy(() => import('./components/Header'));
+const Footer      = lazy(() => import('./components/Footer'));
+const Overview    = lazy(() => import('./pages/Overview'));
+const About       = lazy(() => import('./pages/About'));
+const Admin       = lazy(() => import('./pages/Admin'));
+const Form        = lazy(() => import('./pages/Form'));
+const Login       = lazy(() => import('./pages/Login'));
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="bg-gray-900 text-white grid grid-flow-row grid-rows-[min-content_1fr] h-screen ">
+      <Header/>
+      <Routes>
+        <Route path="/" component={Overview}/>
+        <Route path="/about" component={About}/>
+        <Route path="/admin" component={Admin}/>
+        <Route path="/form" component={Form}/>
+        <Route path="/login" component={Login}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
