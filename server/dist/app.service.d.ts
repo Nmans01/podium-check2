0ccbd141c2292858c73d41eb11de808dc5b93697
nmans01 <natemans01@gmail.com>
2023-03-28 11:39:02 -0400

    Client init

---
+++ server/dist/app.service.d.ts
@@ -0,0 +1,7 @@
+import { PrismaService } from 'nestjs-prisma';
+export declare class AppService {
+    private prisma;
+    constructor(prisma: PrismaService);
+    getUsers(): any;
+    getHello(): string;
+}
