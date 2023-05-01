aa5500f6937cbfdc49c5730e9a236971105f37a6
nmans01 <natemans01@gmail.com>
2023-05-01 19:06:05 -0400

    Bingus

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -1,19 +1,15 @@
-import { createSignal, onMount } from "solid-js";
+import { createSignal, onMount, createResource } from "solid-js";
 import { A } from "@solidjs/router";
-import { getMessage } from "../utils/getMessage";
+import { API_BASE_URL } from "../../config.js";
 
 function Overview() {
 
-    const [message, setMessage] = createSignal({
-        text:"Loading...",
-        actions:[{
-            text:`About this site`,
-            ref:"#"
-        }]
-    });
+    const [message, setMessage] = createSignal();
 
-    onMount(async () => {
-        setMessage(await getMessage());
+    const [messageGet] = createResource(message, async () => {
+        const response = await fetch(API_BASE_URL + '/message');
+        const data = await response.json();
+        return data;
     });
 
     return (
@@ -21,11 +17,11 @@ function Overview() {
             <section class="rounded-sm p-2 flex flex-col gap-5 text-3xl">
                 <span class="text-8xl font-bold">Welcome back!</span>
                 <p>
-                    {message().text}
+                    {messageGet().text}
                 </p>
                 <div class="flex gap-2">
-                    {message().actions.map((action)=>
-                    <A href={action.ref} class="bg-slate-300 text-slate-900 p-2 text-sm rounded-sm hover:brightness-90 active:brightness-75">{action.text}</A>
+                    {messageGet().actions.map((action) =>
+                        <A href={action.ref} class="bg-slate-300 text-slate-900 p-2 text-sm rounded-sm hover:brightness-90 active:brightness-75">{action.text}</A>
                     )}
                 </div>
             </section>
