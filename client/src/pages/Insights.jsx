439ac769067d423ec491cb615641d8d5e3da2263
Nick <noltn@etown.edu>
2023-05-01 20:18:34 -0400

    reworked insights code

--- client/src/pages/Insights.jsx
+++ client/src/pages/Insights.jsx
@@ -1,86 +1,90 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
-    <main class="bg-slate-700 mx-2 p-2 rounded-md flex flex-col gap-2 max-w-4xl justify-self-center">
-        <h2 class="text-2xl">About</h2>
-        <p>
-            Testing. Welcome to the page!
-        </p>
-    </main>
+const buildings = [
+  {
+    name: 'BSC',
+    rooms: ['200', '233'],
+  },
+  {
+    name: 'Eshade',
+    rooms: ['163', '164', '182', '184', '187', '260H-lab', '270', '273', '280- key', '281', '360', '362', '368', '370', '373', '380', '382'],
+  },
+  {
+    name: 'Hoover',
+    rooms: ['107', '108-lab', '109', '110', '111-lab', '112', '114', '211', '212', '213', '214', '215'],
+  },
+  {
+    name: 'Lyet',
+    rooms: ['150', '151', '152', '153', '249', '250', '251', '252', '253'],
+  },
+  {
+    name: 'Musser',
+    rooms: ['108', '215', '232'],
+  },
+  {
+    name: 'Nicarry',
+    rooms: ['104', '106', '127', '129', '131', '201', '202', '203-lab', '204', '205', '206', '207', '208-lab', '210', '212', '228', '230', '232', '234'],
+  },
+  {
+    name: 'Steinman',
+    rooms: ['101', '204', '210', '211'],
+  },
+  {
+    name: 'Thompson',
+    rooms: ['105'],
+  },
+  {
+    name: 'Wenger',
+    rooms: ['001', '204', '211', '304', '310'],
+  },
+  {
+    name: 'Zug',
+    rooms: ['102', '114', '116', '125', '126', '204'],
+  },
+];
 
-function Insights() {
-  const [building, setBuilding] = useState("");
-  const [room, setRoom] = useState("");
+const BuildingRoomSelector = () => {
+  const [selectedBuilding, setSelectedBuilding] = useState('');
+  const [selectedRoom, setSelectedRoom] = useState('');
 
   const handleBuildingChange = (event) => {
-    setBuilding(event.target.value);
+    setSelectedBuilding(event.target.value);
+    setSelectedRoom('');
   };
 
   const handleRoomChange = (event) => {
-    setRoom(event.target.value);
+    setSelectedRoom(event.target.value);
   };
 
   return (
     <div>
-      <label htmlFor="building-select">Select a building:</label>
-      <select id="building-select" value={building} onChange={handleBuildingChange}>
-        <option value="">-- Select a building --</option>
-        <option value="BSC">BSC</option>
-        <option value="Esbenshade">Esbenshade</option>
-        <option value="Hoover">Hoover</option>
-        <option value="Lyet">Lyet</option>
-        <option value="Musser">Musser</option>
-        <option value="Nicarry">Nicarry</option>
-        <option value="Steinman">Steinman</option>
-        <option value="Thompson">Thompson</option>
-        <option value="Wenger">Wenger</option>
-        <option value="Zug">Zug</option>
+      <label htmlFor="building-selector">Select a building:</label>
+      <select id="building-selector" value={selectedBuilding} onChange={handleBuildingChange}>
+        <option value="">--Select a building--</option>
+        {buildings.map((building) => (
+          <option key={building.name} value={building.name}>
+            {building.name}
+          </option>
+        ))}
       </select>
 
-      <label htmlFor="room-select">Select a room:</label>
-      <select id="room-select" value={room} onChange={handleRoomChange}>
-        <option value="">-- Select a room --</option>
-        {building === "Nicarry" && (
-          <>
-            <option value="N104">104</option>
-            <option value="N106">106</option>
-            <option value="N127">127</option>
-            <option value="N129">129</option>
-            <option value="N131">131</option>
-            <option value="N201">201</option>
-            <option value="N202">202</option>
-            <option value="N203-lab">203-lab</option>
-            <option value="N204">204</option>
-            <option value="N205">205</option>
-            <option value="N206">206</option>
-            <option value="N207">207</option>
-            <option value="N208-lab">208-lab</option>
-            <option value="N210">210</option>
-            <option value="N212">212</option>
-            <option value="N228">228</option>
-            <option value="N230">230</option>
-            <option value="N232">232</option>
-            <option value="N234">234</option>
-          </>
-        )}
-        {building === "Hoover" && (
-          <>
-            <option value="H107">107</option>
-            <option value="H108-lab">108-lab</option>
-            <option value="H109">109</option>
-            <option value="H110">110</option>
-            <option value="H111-lab">111-lab</option>
-            <option value="H112">112</option>
-            <option value="H114">114</option>
-            <option value="H211">211</option>
-            <option value="H212">212</option>
-            <option value="H213">213</option>
-            <option value="H214">214</option>
-            <option value="H215">215</option>
-          </>
-        )}
-      </select>
+      {selectedBuilding && (
+        <div>
+          <label htmlFor="room-selector">Select a room:</label>
+          <select id="room-selector" value={selectedRoom} onChange={handleRoomChange}>
+            <option value="">--Select a room--</option>
+            {buildings.find((building) => building.name === selectedBuilding).rooms.map((room) => (
+              <option key={room} value={selectedBuilding.charAt(0) + room}>
+                {selectedBuilding.charAt(0) + room}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+
+      {selectedRoom && <p>You have selected room: {selectedRoom}</p>}
     </div>
   );
-}
+};
 
-export default Insights;
+export default BuildingRoomSelector;
