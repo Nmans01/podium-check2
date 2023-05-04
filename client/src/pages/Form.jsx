8524fdcfae44ba0f690ca9870c08e003673aee10
Seamus Corr <corrs@etown.edu>
2023-05-04 13:37:41 -0400

    Form page basics

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -4,7 +4,8 @@ function Form() {
     // TODO: This will be the page where employees fill out the podium check form. It should use the <form> element and have checkbox <input>s.
     return (
         <main class="bg-slate-700 mx-2 p-2 rounded-md flex flex-col gap-2">
-            <h2 class="text-2xl">Form</h2>
+            <h2 class="text-4xl">Select Form</h2>
+            <button style="background-color:#788693; height:100px; width:300px; font-size: 50px;">E281</button>
         </main>
     );
 }
