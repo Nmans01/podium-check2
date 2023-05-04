c69fdaf686b844333ec2bb30e8b57663f285000d
Seamus Corr <corrs@etown.edu>
2023-05-04 14:32:38 -0400

    Admin Basics

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -4,7 +4,19 @@ function Admin() {
     // TODO: This page should use <table> to show admins lists of database tables such as users and rooms. Admins should be able to add and delete database entries here.
     return (
         <main class="bg-slate-700 mx-2 p-2 rounded-md flex flex-col gap-2">
-            <h2 class="text-2xl">Admin</h2>
+            <h2 class="text-4xl">Admin</h2>
+            <table style="background-color:grey; height:400px;">
+                <tbody>
+                    <tr>
+                        <th>Users</th>
+                        <th>User #</th>
+                    </tr>
+                    <tr>
+                        <td>Row 2 Col 1</td>
+                        <td>Row 2 Col 2</td>
+                    </tr>
+                </tbody>
+            </table>
         </main>
     );
 }
