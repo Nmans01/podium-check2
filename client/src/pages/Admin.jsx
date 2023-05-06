ba22936d0bcc059c889b127bfb9283dbba6b6c7f
Seamus Corr <corrs@etown.edu>
2023-05-06 15:40:51 -0400

    Reformated Admin table

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -5,16 +5,17 @@ function Admin() {
     return (
         <main class="bg-slate-700 mx-2 p-2 rounded-md flex flex-col gap-2">
             <h2 class="text-4xl">Admin</h2>
-            <table style="background-color:grey; height:400px;">
+            <table style="background-color:grey; height:auto; width:600px; border: 2px solid black; border-collapse: collapse; marhin-left:100px; position:relative; left:100px; top:50px;">
                 <tbody>
                     <tr>
-                        <th>Users</th>
-                        <th>User #</th>
+                        <th style="height:100px; border: 2px solid black; border-collapse: collapse; font-size:35px">Users</th> 
+                        <th style="border: 2px solid black; border-collapse: collapse; font-size:35px">User #</th>
                     </tr>
                     <tr>
-                        <td>Row 2 Col 1</td>
-                        <td>Row 2 Col 2</td>
+                        <td style="border: 2px solid black; border-collapse: collapse">Row 2 Col 1</td>
+                        <td style="border: 2px solid black; border-collapse: collapse; text-allign:center">Row 2 Col 2</td>
                     </tr>
+                    
                 </tbody>
             </table>
         </main>
