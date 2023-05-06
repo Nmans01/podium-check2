61b98a47f4746fff34aac77b5d7b524a4e75a14c
nmans01 <natemans01@gmail.com>
2023-05-06 15:17:50 -0400

    Remove tRPC

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessageDto } from './dtos/message';
 
-@Controller('api')
+Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
