5d43cd7b2f73f5a0fc82e9124c2a3086297ac647
Seamus Corr <corrs@etown.edu>
2023-05-07 14:44:24 -0400

    Advanced Assignment buttons

--- client/src/pages/Assignments.jsx
+++ client/src/pages/Assignments.jsx
@@ -6,10 +6,26 @@ function Assignments() {
     return (
         <main class="bg-slate-700 mx-2 p-2 rounded-md flex flex-col gap-2">
             <h2 class="text-4xl">Assignments</h2>
-            <button style="background-color:#788693; height:100px; width:400px; font-size: 50px; position:relative;">Esbenshade</button>
+            <button style="background-color:#788693; height:100px; width:400px; font-size: 45px; float:left;">
+                Esbenshade
+                <div style="background-color:#EADD66; width:50px; height:50px; border-radius:25px; border:3px solid white; float:right; margin-right:25px; margin-top:10px; font-size: 30px;">
+                    <span>
+                        3
+                    </span>
+                </div>
+            </button>
             <hr style="background-color:black; height:5px; border: none; width:95%; margin: auto"/>
-            <button style="background-color:#788693; height:80px; width:250px; font-size: 50px; position:relative;">Nick</button>
-            <button style="background-color:#788693; height:100px; width:250px; font-size: 50px; position:relative;">Submit</button>
+            <button style="background-color:#788693; height:80px; width:250px; font-size: 50px; position:relative; float:left;">
+                Nick
+                <div style="background-color:#A2E1A5; width:85px; height:50px; border-radius:25px; border:3px solid white; float:right; margin-right:25px; margin-top:15px; font-size: 30px;">
+                    <span>
+                        Esb
+                    </span>
+                </div>
+            </button>
+
+
+            <button style="background-color:#788693; height:100px; width:250px; font-size: 50px; position:relative; left:1000px">Submit</button>
         </main>
     );  
 }
