18b7e1b49cf0b35fd0d72feee2f7d1163cfac658
Seamus Corr <corrs@etown.edu>
2023-05-07 22:07:44 -0400

    Form advanced buttons

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -5,7 +5,14 @@ function Form() {
     return (
         <main class="bg-slate-700 mx-2 p-2 rounded-md flex flex-col gap-2">
             <h2 class="text-4xl">Select Form</h2>
-            <button style="background-color:#788693; height:100px; width:300px; font-size: 50px; postition:relative">E281</button>
+            <button style="background-color:#788693; height:100px; width:300px; font-size: 50px; postition:relative">
+                E281
+                <div style="background-color:#A2E1A5; width:50px; height:50px; border-radius:25px; border:3px solid white; float:right; margin-right:25px; margin-top:10px; font-size: 30px;">
+                    <span>
+                        7
+                    </span>
+                </div>
+            </button>
         </main>
     );
 }
